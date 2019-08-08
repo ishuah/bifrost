773a95180b1a09aacae74cb80218835c98edadd2
ishuah <kariuki@ishuah.com>
2019-08-08 17:10:20 +0300

    variable declaration for test input

--- connect_test.go
+++ connect_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -10,10 +11,20 @@ func TestNewConnection(t *testing.T) {
 
 	port0 := "/dev/nonexistentport"
 	port1 := "/tmp/bifrostmaster"
+	port2 := "/tmp/bifrostslave"
+	testInput := []byte("from Asgard...\n")
 
 	_, err := NewConnection(port0, 115200)
 	require.Error(t, err)
 
-	_, err = NewConnection(port1, 115200)
+	connect1, err := NewConnection(port1, 115200)
 	require.NoError(t, err)
+
+	connect2, err := NewConnection(port2, 115200)
+	require.NoError(t, err)
+
+	connect1.Write(testInput)
+	message1, err := connect2.portReader.ReadBytes('\n')
+	require.NoError(t, err)
+	assert.Equal(t, testInput, message1)
 }
