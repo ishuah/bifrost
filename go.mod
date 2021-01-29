1dffa25820656a146d98c30fa997d8ea6ee6401c
Ishuah Kariuki <kariuki@ishuah.com>
2021-01-29 11:47:02 +0300

    Feature: Config file
    Implemented both save and load functions

--- go.mod
+++ go.mod
@@ -6,7 +6,8 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/micmonay/keybd_event v0.0.0-20190804163414-202c5b2c8150
 	github.com/pkg/term v0.0.0-20180423043932-cda20d4ac917
+	github.com/smartystreets/goconvey v1.6.4 // indirect
 	github.com/stretchr/testify v1.3.0
 	github.com/tarm/serial v0.0.0-20180114052751-eaafced92e96
-	golang.org/x/sys v0.0.0-20180202135801-37707fdb30a5 // indirect
+	gopkg.in/ini.v1 v1.62.0
 )
--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"strconv"
+
+	"gopkg.in/ini.v1"
 )
 
 const version = "v0.2.1"
@@ -17,9 +22,14 @@ Bifrost is a tiny terminal emulator for serial port communication.
 	  
     Flags:
       -port-path	Name/path of the serial port
-      -baud		The baud rate to use on the connection
-      -help		This help message
+      -baud			The baud rate to use on the connection
+      -save-config	Save the connection configuration
+      -help			This help message
 	`, header)
+
+var configDir = os.Getenv("HOME") + "/.bifrost/"
+
+const configFile = "config.ini"
 
 func welcomeMessage(portPath string, baud int) string {
 	return fmt.Sprintf(`%s
@@ -31,14 +41,95 @@ Press Ctrl+\ to exit
 		`, header, portPath, baud)
 }
 
+func writeConfig(configName string, portPath string, baud int) error {
+	configPath := configDir + configFile
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		err = os.Mkdir(configDir, os.ModePerm)
+		if err != nil {
+			return err
+		}
+
+		err = ioutil.WriteFile(configPath, []byte(""), os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+
+	config, err := ini.Load(configPath)
+	if err != nil {
+		return err
+	}
+
+	_, err = config.NewSection(configName)
+	if err != nil {
+		return err
+	}
+
+	config.Section(configName).Key("port").SetValue(portPath)
+	config.Section(configName).Key("baud").SetValue(strconv.Itoa(baud))
+
+	config.SaveTo(configPath)
+	return nil
+}
+
+func readConfig(configName string) (portPath string, baud int, err error) {
+	configPath := configDir + configFile
+
+	config, err := ini.Load(configPath)
+	if err != nil {
+		return
+	}
+
+	section, err := config.GetSection(configName)
+	if err != nil {
+		return
+	}
+
+	portPath = section.Key("port").String()
+	baud, _ = section.Key("baud").Int()
+
+	return
+}
+
 func main() {
 	var portPath string
 	var baud int
+	var saveConfig bool
+	var loadConfig string
 	var help bool
+
 	flag.StringVar(&portPath, "port-path", "/dev/tty.usbserial", "Name/path of the serial port")
 	flag.IntVar(&baud, "baud", 115200, "The baud rate to use on the connection")
+	flag.BoolVar(&saveConfig, "save-config", false, "Save the connection configuration")
+	flag.StringVar(&loadConfig, "load-config", "", "Load a connection configuration to start a connection")
 	flag.BoolVar(&help, "help", false, "A brief help message")
 	flag.Parse()
+
+	if saveConfig {
+		var configName string
+		fmt.Println("What name do you want to save this config under?")
+		fmt.Scanln(&configName)
+		err := writeConfig(configName, portPath, baud)
+		if err != nil {
+			log.Printf("Failed to save config. FatalError: %v\n", err)
+			return
+		}
+		fmt.Printf("Config saved! You can view and edit your configurations at %s%s.\n", configDir, configFile)
+		return
+	}
+
+	if loadConfig != "" {
+		fmt.Printf("Loading config %s...\n", loadConfig)
+		cfgPortPath, cfgBaud, err := readConfig(loadConfig)
+		if err != nil {
+			log.Printf("Failed to load config. FatalError: %v\n", err)
+			return
+		}
+		portPath = cfgPortPath
+		baud = cfgBaud
+		fmt.Println("Config loaded successfully.")
+	}
 
 	if help {
 		fmt.Println(helpText)
@@ -47,7 +138,7 @@ func main() {
 
 	connect, err := NewConnection(portPath, baud)
 	if err != nil {
-		log.Printf("FatalError: %v", err)
+		log.Printf("FatalError: %v\n", err)
 		return
 	}
 	// Welcome message
