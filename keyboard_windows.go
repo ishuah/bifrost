c0e0eeeba36894f072771df6ddf9380485559913
Ishuah Kariuki <kariuki@ishuah.com>
2025-03-26 12:21:06 +0300

    Added keyboard actions

--- keyboard_windows.go
+++ keyboard_windows.go
@@ -17,6 +17,26 @@ func KeyboardListener(connect *Connect) {
 		case keys.CtrlBackslash:
 			fmt.Println("\nbye!")
 			return true, nil
+		case keys.Tab:
+			connect.Write([]byte{'\x09'})
+		case keys.CtrlC:
+			connect.Write([]byte{'\x03'})
+		case keys.Enter:
+			connect.Write([]byte{'\r'})
+		case keys.Backspace:
+			connect.Write([]byte{'\x08'})
+		case keys.Delete:
+			connect.Write([]byte{'\x08'})
+		case keys.Left:
+			connect.Write([]byte{'\x1b', '[', 'D'})
+		case keys.Right:
+			connect.Write([]byte{'\x1b', '[', 'C'})
+		case keys.Up:
+			connect.Write([]byte{'\x1b', '[', 'A'})
+		case keys.Down:
+			connect.Write([]byte{'\x1b', '[', 'B'})
+		case keys.Space:
+			connect.Write([]byte{' '})
 		case keys.RuneKey:
 			connect.Write([]byte(string(key.Runes)))
 		}
